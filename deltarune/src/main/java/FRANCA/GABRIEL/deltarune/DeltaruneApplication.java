1f9f8ed29cf6a3844ab1e49fed7c4a78ea6bfa0f
gabrielfranca42 <gabrielfranca172@gmail.com>
2025-08-25 10:14:39 -0300

    first commit

---
+++ deltarune/src/main/java/FRANCA/GABRIEL/deltarune/DeltaruneApplication.java
@@ -0,0 +1,13 @@
+package FRANCA.GABRIEL.deltarune;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeltaruneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeltaruneApplication.class, args);
+	}
+
+}
---
+++ deltarune/src/test/java/FRANCA/GABRIEL/deltarune/DeltaruneApplicationTests.java
@@ -0,0 +1,13 @@
+package FRANCA.GABRIEL.deltarune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeltaruneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
